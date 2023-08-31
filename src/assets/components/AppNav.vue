d599b7000de25fed550e60d072a59614f68daa1a
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-08-31 17:58:03 +0200

    add navbar and Jumbo (temporary)

--- src/assets/components/AppNav.vue
+++ src/assets/components/AppNav.vue
@@ -10,6 +10,31 @@ export default {
 
 </script>
 
-<template></template>
+<template>
+    <nav>
+        <div class="container">
+            <div class="logo">
+                <img src="../img/coloratofinale_360.png" alt="">
+            </div>
 
-<style></style>
+            <div class="buttons">
+                <button type="button" class="btn btn-light">Registrati</button>
+                <button type="button" class="btn btn-light">Accedi</button>
+                <button type="button" class="btn btn-light">Carrello</button>
+            </div>
+        </div>
+    </nav>
+    <div class="jumbo">
+        <img src="../img/jumbo-deliveboo.png" alt="">
+    </div>
+</template>
+
+<style>
+*,
+::after,
+::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+</style>
