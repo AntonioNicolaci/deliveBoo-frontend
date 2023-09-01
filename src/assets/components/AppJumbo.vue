658dd00a3fd78e06fd3f6e9a575cedb19e2e2f9a
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-09-01 16:49:30 +0200

    add jumbo component

---
+++ src/assets/components/AppJumbo.vue
@@ -0,0 +1,32 @@
+<script>
+
+export default {
+    data() {
+        return {
+
+        };
+    },
+}
+
+</script>
+
+<template>
+    <div class="jumbo">
+        <img src="../img/jumbo-deliveboo.png" alt="">
+    </div>
+</template>
+
+<style>
+*,
+::after,
+::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.jumbo img {
+    width: 100%;
+    background-size: cover;
+}
+</style>
