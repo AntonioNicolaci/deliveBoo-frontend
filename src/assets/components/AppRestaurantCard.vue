d2dbed0fb96e1a5f4c6eb88f747d71fad517717f
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-09-04 12:32:16 +0200

    fix sintax

--- src/assets/components/AppRestaurantCard.vue
+++ src/assets/components/AppRestaurantCard.vue
@@ -20,12 +20,12 @@ export default {
     <div class="card mb-3" style="max-width: 540px;">
         <div class="row g-0">
             <div class="col-md-4">
-                <img :src="rest.img" class="img-fluid rounded-start" :alt="rest.name">
+                <img :src="rest.img" class="img-fluid rounded-start" :alt="restaurant.name">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
-                    <h5 class="card-title">{{ rest.rest_name }}</h5>
-                    <p class="card-text">{{ rest.address }}</p>
+                    <h5 class="card-title">{{ restaurant.rest_name }}</h5>
+                    <p class="card-text">{{ restaurant.address }}</p>
                 </div>
             </div>
         </div>
