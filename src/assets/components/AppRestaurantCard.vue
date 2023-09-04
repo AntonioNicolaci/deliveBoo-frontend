fd0a57fae781a38067895580d85ce8b910ad9c8c
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-09-04 12:26:41 +0200

    add card

--- src/assets/components/AppRestaurantCard.vue
+++ src/assets/components/AppRestaurantCard.vue
@@ -17,15 +17,31 @@ export default {
 </script>
 
 <template>
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img :src="rest.img" class="img-fluid rounded-start" :alt="rest.name">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">{{ rest.rest_name }}</h5>
+                    <p class="card-text">{{ rest.address }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- 
+
     <div class="cont-card">
         <div class="card border" style="width: 18rem">
             <img :src="rest.img" class="card-img-top" :alt="rest.name" />
             <div class="card-body">
                 <h2 class="card-text">{{ rest.rest_name }}</h2>
-                <h3>{{ rest.types }}</h3>
+                <h3>{{ rest.address }}</h3>
             </div>
         </div>
-    </div>
+    </div> -->
 </template>
 
 <style lang="scss"></style>
