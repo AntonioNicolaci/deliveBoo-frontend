23e20f775615b2030ce93b117e00a70853f28d80
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-09-05 15:07:26 +0200

    fix restaurant card responsive

--- src/assets/components/AppRestaurantCard.vue
+++ src/assets/components/AppRestaurantCard.vue
@@ -13,23 +13,15 @@ export default {
 </script>
 
 <template>
-  <div class="card-gap">
-    <div class="card mb-3">
-      <div class="row g-0">
-        <div class="col-md-4 cont-img">
-          <img
-            :src="img"
-            class="img-fluid rounded-start img"
-            :alt="restaurant.name"
-          />
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h5 class="card-title">
-              {{ restaurant.rest_name }}
-            </h5>
-            <p class="card-text">{{ restaurant.address }}</p>
-          </div>
+  <div class="card mb-3" style="max-width: 540px;">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img :src=img class="img-fluid rounded-start" :alt="restaurant.name">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">{{ restaurant.rest_name }}</h5>
+          <p class="card-text">{{ restaurant.address }}</p>
         </div>
       </div>
     </div>
@@ -48,14 +40,14 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-.card-gap {
-  flex: 0 0 30%;
-  padding: 0.5rem 0.8rem;
+// .restaurantcard {
+//   flex: 0 0 30%;
+//   padding: 0.5rem 0.8rem;
 
-  img {
-    width: 100%;
-    background-size: cover;
-    background-position: center;
-  }
-}
+//   img {
+//     width: 100%;
+//     background-size: cover;
+//     background-position: center;
+//   }
+// }
 </style>
