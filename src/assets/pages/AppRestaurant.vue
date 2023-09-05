d7ff75187c410de2624dea3d74bab8402544c178
antonionicolaci <antonionicolaci48@gmail.com>
2023-09-05 19:56:01 +0200

    fix multiple plate

--- src/assets/pages/AppRestaurant.vue
+++ src/assets/pages/AppRestaurant.vue
@@ -11,39 +11,40 @@ export default {
     methods: {
         addPlate(id) {
             let cart = localStorage.getItem("cart")
-            let quant = document.getElementById(id).value
+            let quantit = document.getElementById(id).value
             document.querySelectorAll(".text-danger").forEach(element => {
                 element.innerHTML = ""
             })
-            if (quant <= 0 || quant == null) {
+            if (quantit <= 0 || quantit == null) { // Controlla se il valore del campo di input è 0 o un numero negativo
                 document.getElementById(`error`+id).innerHTML = 'valore minimo richiesto "1"'
             } else {
-                if (cart !== null) {
-                    if (cart.includes('{"id":"'+ id) == true) {
-                        let obj = JSON.parse(cart)
-                        obj.forEach(element => {
-                           if(element.id == id) {
-                            console.log(element.quant);
-                            element.quant = quant
-                           } 
-                        });
-                        console.log(obj);
-                        // .replace('{"id":"'+ id + '", "quant":"'+ quant +'"}', '{"id":"'+ id + '", "quant":"'+ quantChange +'"}')
-                        console.log("oggetto aggiunto 1");
-                    } else {    
+                if (cart !== null) { // Controlla se il carrello è vuoto o meno
+                    if (cart.includes('{"id":"'+ id) == true) { // Controlla se l'id è già presente
+                    JSON.parse(cart).forEach((element, key) => {
+                        if (element.id == id) {
+                            let carrello = JSON.parse(cart)
+                            element.quant = quantit
+                            carrello[key] = element
+                            console.dir(carrello);
+                            localStorage.setItem("cart", JSON.stringify(carrello))
+                            console.log(localStorage.getItem("cart"));    
+                        }
+                    })
+                    } else {
                         cart = cart.replace("]", ",")
-                        cart += '{"id":"'+ id + '", "quant":"'+ quant +'"}]'
+                        cart += '{"id":"'+ id + '", "quant":"'+ quantit +'"}]'
+                        cart = cart
 
                         localStorage.setItem("cart", cart)
 
-                        console.log("oggetto aggiunto 2");
+                        console.log("oggetto aggiunto");
                     }
                 } else {
-                    let cart = '[{"id":"'+ id + '", "quant":"'+ quant +'"}]'
+                    cart = '[{"id":"'+ id + '", "quant":"'+ quantit +'"}]'
                     localStorage.setItem("cart", cart)
                     console.log("oggetto creato 3");
                 }
-                this.cart = JSON.parse(cart)    
+                this.cart = JSON.parse(cart)  
             }
             
         },
@@ -68,6 +69,7 @@ export default {
     <button @click="destroyStorage()" style="background-color: red; width: 100px; height: 50px; color: aliceblue;">Nuke</button>
     <div v-for="plate in plates">
         <div class="d-flex">
+            <span>{{ plate.name }}</span>
             <input type="number" :name="plate.id" :id="plate.id">
             <div style="width:30px; height: 30px; background-color: black; margin: 5px;" @click="addPlate(plate.id)"></div>
             <span class="text-danger" style="font-weight: bold;" :id="`error${ plate.id }`"></span>
